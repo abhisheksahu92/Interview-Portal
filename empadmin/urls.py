082a95bafec2f94c22eecfc8d02be339eeaecbe7
Abhishek Sahu <abhisheksahu92@outlook.com>
2024-05-03 17:36:13 +0530

    Fixed the URL for Resume and in Urls.py file.

--- empadmin/urls.py
+++ empadmin/urls.py
@@ -5,7 +5,7 @@ app_name = 'empadmin'
 urlpatterns = [
     path('',views.adminIndexView,name='admin-index'),
     path('status/<str:status>',views.adminStatusList,name='admin-status'),
-    path('canidatelist/?page=n',views.adminCandidateListView,name='admin-candidate-list'),
+    path('candidatelist/?page=n',views.adminCandidateListView,name='admin-candidate-list'),
     path('employeelist/?page=n',views.adminEmployeeeListView,name='admin-employee-list'),
     path('login/',views.adminLoginView,name='admin-login'),
     path('logout/',views.adminLogoutView,name='admin-logout'),
