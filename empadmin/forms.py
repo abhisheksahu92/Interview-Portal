0b5f50b55f5999ce9353d3322287135282775c1d
Abhishek Sahu <abhisheksahu92@outlook.com>
2024-05-04 17:49:14 +0530

    IP-80   Employee Page Updates for Admin.

--- empadmin/forms.py
+++ empadmin/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from candidate.models import Candidate
+from employee.models import Employee
 
 class AdminLoginForm(forms.Form):
     username = forms.CharField(label = 'Username', max_length=120, required=True)
@@ -16,3 +17,15 @@ class AdminUpdateForm(forms.ModelForm):
     class Meta:
         model = Candidate
         fields = ['phone', 'email']
+
+class AdminEmployeeUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['employee_name', 'employee_email', 'employee_skill', 'employee_level']
+
+        def clean(self):
+            cleaned_data = super().clean()
+            email = cleaned_data.get('employee_email')
+            if email != self.instance.employee_email:
+                if Employee.objects.filter(employee_email=email).exists():
+                    raise forms.ValidationError('Employee with this Official Email already exists.')
